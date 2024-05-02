b2f8139ef8314f1cd53fd86c4ceea4c223c4a407
Lucas Celse <lucas.j.celse@gmail.come>
2024-05-02 13:14:59 -0300

    prueba

---
+++ assets/js/envio.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('button');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    btn.value = 'Enviando...';
+
+    const serviceID = 'service_uz6l58l';
+    const templateID = 'template_jcarmjq';
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email';
+            alert('Enviado!');
+            form.reset(); 
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert(JSON.stringify(err));
+            form.reset(); 
+        });
+})
